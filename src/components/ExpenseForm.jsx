0c9ff9e28dff0e70e5e86250bbdac343573a6556
Mark-crypto <Mark.Onyango@strathmore.edu>
2025-03-17 10:30:33 +0300

    navbar UI

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -5,6 +5,7 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { useFormik } from "formik";
 import expenseValidation from "../schemas/expenseValidation";
+import Navbar from "./Navbar.jsx";
 
 const ExpenseForm = () => {
   const [expense, setExpense] = useState({
@@ -29,105 +30,119 @@ const ExpenseForm = () => {
   return (
     <div
       style={{
-        backgroundColor: "#9D00FF",
-        height: "100vh",
         display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "space-between",
       }}
     >
-      <div>
-        <Form
-          onSubmit={addExpense}
-          style={{
-            width: "500px",
-            margin: "auto",
-            border: "1px solid grey",
-            padding: "20px",
-            borderRadius: "10px",
-            backgroundColor: "white",
-          }}
-        >
-          <h3
+      <div style={{ width: "20%" }}>
+        <Navbar />
+      </div>
+      <div
+        style={{
+          backgroundColor: "#9D00FF",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "80%",
+        }}
+      >
+        <div>
+          <Form
+            onSubmit={addExpense}
             style={{
-              color: "#9D00FF",
-              marginBottom: "20px",
-              textAlign: "center",
+              width: "500px",
+              margin: "auto",
+              border: "1px solid grey",
+              padding: "20px",
+              borderRadius: "10px",
+              backgroundColor: "white",
             }}
           >
-            Add Expense
-          </h3>
-          <Form.Group className="mb-3">
-            <Form.Label>Amount</Form.Label>
-            <Form.Control
-              type="number"
-              placeholder="Enter amount"
-              value={expense.amount}
-              onChange={(e) =>
-                setExpense({ ...expense, amount: e.target.value })
-              }
-              onBlur={formik.handleBlur}
-              name="amount"
-            />
-            {formik.errors.amount && formik.touched.amount && (
-              <p style={{ color: "red" }}>{formik.errors.amount}</p>
-            )}
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Category</Form.Label>
-            <Form.Select
-              value={expense.category}
-              onChange={(e) =>
-                setExpense({ ...expense, category: e.target.value })
-              }
-              name="category"
-              onBlur={formik.handleBlur}
+            <h3
+              style={{
+                color: "#9D00FF",
+                marginBottom: "20px",
+                textAlign: "center",
+              }}
             >
-              <option>Choose a Category</option>
-              <option value="clothing">Clothing</option>
-              <option value="debt">Debt</option>
-              <option value="education">Education</option>
-              <option value="entertainment">Entertainment</option>
-              <option value="food">Food</option>
-              <option value="gifts">Gifts</option>
-              <option value="healthcare">Healthcare</option>
-              <option value="housing">Housing</option>
-              <option value="householdSupplies">Household Supplies</option>
-              <option value="insurance">Insurance</option>
-              <option value="personal">Personal</option>
-              <option value="retirement">Retirement</option>
-              <option value="transportation">Transportation</option>
-              <option value="utilities">Utilities</option>
-            </Form.Select>
-            {formik.errors.category && formik.touched.category && (
-              <p style={{ color: "red" }}>{formik.errors.category}</p>
-            )}
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Date</Form.Label>
-            <Form.Control
-              type="date"
-              placeholder=""
-              value={expense.date}
-              onChange={(e) => setExpense({ ...expense, date: e.target.value })}
-              onBlur={formik.handleBlur}
-              name="date"
-            />
-            {formik.errors.date && formik.touched.date && (
-              <p style={{ color: "red" }}>{formik.errors.date}</p>
-            )}
-          </Form.Group>
-          <Button
-            style={{
-              backgroundColor: "#9D00FF",
-              marginBottom: "20px",
-              width: "100%",
-            }}
-            type="submit"
-          >
-            Add Expense
-          </Button>
-        </Form>
+              Add Expense
+            </h3>
+            <Form.Group className="mb-3">
+              <Form.Label>Amount</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter amount"
+                value={expense.amount}
+                onChange={(e) =>
+                  setExpense({ ...expense, amount: e.target.value })
+                }
+                onBlur={formik.handleBlur}
+                name="amount"
+              />
+              {formik.errors.amount && formik.touched.amount && (
+                <p style={{ color: "red" }}>{formik.errors.amount}</p>
+              )}
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Category</Form.Label>
+              <Form.Select
+                value={expense.category}
+                onChange={(e) =>
+                  setExpense({ ...expense, category: e.target.value })
+                }
+                name="category"
+                onBlur={formik.handleBlur}
+              >
+                <option>Choose a Category</option>
+                <option value="clothing">Clothing</option>
+                <option value="debt">Debt</option>
+                <option value="education">Education</option>
+                <option value="entertainment">Entertainment</option>
+                <option value="food">Food</option>
+                <option value="gifts">Gifts</option>
+                <option value="healthcare">Healthcare</option>
+                <option value="housing">Housing</option>
+                <option value="householdSupplies">Household Supplies</option>
+                <option value="insurance">Insurance</option>
+                <option value="personal">Personal</option>
+                <option value="retirement">Retirement</option>
+                <option value="transportation">Transportation</option>
+                <option value="utilities">Utilities</option>
+              </Form.Select>
+              {formik.errors.category && formik.touched.category && (
+                <p style={{ color: "red" }}>{formik.errors.category}</p>
+              )}
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Date</Form.Label>
+              <Form.Control
+                type="date"
+                placeholder=""
+                value={expense.date}
+                onChange={(e) =>
+                  setExpense({ ...expense, date: e.target.value })
+                }
+                onBlur={formik.handleBlur}
+                name="date"
+              />
+              {formik.errors.date && formik.touched.date && (
+                <p style={{ color: "red" }}>{formik.errors.date}</p>
+              )}
+            </Form.Group>
+            <Button
+              style={{
+                backgroundColor: "#9D00FF",
+                marginBottom: "20px",
+                width: "100%",
+              }}
+              type="submit"
+            >
+              Add Expense
+            </Button>
+          </Form>
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,72 @@
-import React from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { IoMdAdd } from "react-icons/io";
+import { MdSpaceDashboard } from "react-icons/md";
+import { TbBulb } from "react-icons/tb";
+import { FaWallet } from "react-icons/fa";
+import { FaHistory } from "react-icons/fa";
+import { IoMdSettings } from "react-icons/io";
+import { MdLogout } from "react-icons/md";
 
 const Navbar = () => {
   return (
     <>
-      <div>
-        <h1>Expense Tracker</h1>
-        <button>User Profile</button>
+      <div className="navbar">
+        <h3
+          style={{
+            fontStyle: "italic",
+            color: "#9D00FF",
+          }}
+        >
+          Expense Tracker
+        </h3>
+        <button className="btn-nav">
+          <FaUserCircle style={{ marginRight: "8px", fontSize: "22px" }} />
+          User Profile
+        </button>
         <ul>
           <li>
-            <a href="">New Expense</a>
+            <a href="">
+              <IoMdAdd style={{ marginRight: "8px", fontSize: "25px" }} />
+              New Expense
+            </a>
           </li>
           <li>
-            <a href="">Dashboard</a>
+            <a href="">
+              <MdSpaceDashboard
+                style={{ marginRight: "8px", fontSize: "25px" }}
+              />
+              Dashboard
+            </a>
           </li>
           <li>
-            <a href="">Budget</a>
+            <a href="">
+              <TbBulb style={{ marginRight: "8px", fontSize: "25px" }} />
+              AI Predictions
+            </a>
           </li>
           <li>
-            <a href="">History</a>
+            <a href="">
+              <FaWallet style={{ marginRight: "8px", fontSize: "25px" }} />
+              Budget
+            </a>
           </li>
           <li>
-            <a href="">Settings</a>
+            <a href="">
+              <FaHistory style={{ marginRight: "8px", fontSize: "25px" }} />
+              History
+            </a>
           </li>
           <li>
-            <a href="">Logout</a>
+            <a href="">
+              <IoMdSettings style={{ marginRight: "8px", fontSize: "25px" }} />
+              Settings
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <MdLogout style={{ marginRight: "8px", fontSize: "25px" }} />
+              Logout
+            </a>
           </li>
         </ul>
       </div>
