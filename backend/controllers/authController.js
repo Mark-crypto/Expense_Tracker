6c0bee53f09e01c45eaf1d09534e387f3e7d3e12
Mark-crypto <Mark.Onyango@strathmore.edu>
2025-04-21 15:15:51 +0300

    changes in auth

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,41 +1,66 @@
 import connection from "../database.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const signUp = async (req, res) => {
   const { name, email, password } = req.body;
   try {
-    connection.execute(
-      "INSERT INTO users SET name=?, email=?, password=?",
-      [name, email, password],
-      (error, data) => {
-        if (error) {
-          res.status(500).json({ error: true, message: "Error signing up" });
-        }
-        res.status(201).json({ data, message: "User created successfully" });
-      }
+    const isExistingEmail = await connection.execute(
+      "SELECT * FROM users WHERE email = ?",
+      [email]
     );
+    if (isExistingEmail) {
+      return res
+        .status(500)
+        .json({ error: true, message: "Email is already in use." });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const response = await connection.execute(
+      "INSERT INTO users SET name=?, email=?, password=?",
+      [name, email, hashedPassword]
+    );
+    if (response) {
+      return res.status(201).json({ message: "User created successfully" });
+    }
+    return res
+      .status(500)
+      .json({ error: true, message: "An error occurred. Try again later." });
   } catch (error) {
-    res.status(500).json({ error: true, message: "Error signing up" });
+    res.status(500).json({ error: true, message: "An error occurred" });
+    console.log("Error:", error);
   }
 };
 
-export const login = async (req, res) => {};
-
-export const getUser = (req, res) => {
+export const login = async (req, res) => {
   const { email, password } = req.body;
-  const { id } = req.params;
-  connection.execute(
-    "SELECT * FROM users WHERE user_id = ?",
-    [id],
-    (error, data) => {
-      if (error) {
-        res.status(500).json({ error: true, message: "Error fetching user" });
-      }
-      res.status(200).json({ data });
+  try {
+    const user = await connection.execute(
+      "SELECT * FROM users WHERE email = ?",
+      [email]
+    );
+    if (!user) {
+      return res
+        .status(401)
+        .json({ error: true, message: "Invalid email or password" });
     }
-  );
+    const doPasswordsMatch = await bcrypt.compare(password, user.password);
+    if (doPasswordsMatch === false) {
+      return res
+        .status(401)
+        .json({ error: true, message: "Invalid email or password" });
+    }
+    jwt.sign(
+      { userId: user.id, name: user.name, email: user.email },
+      process.env.JWT_SECRET_KEY
+    );
+  } catch (error) {
+    res.status(500).json({ error: true, message: "An error occurred" });
+    console.log("Error:", error);
+  }
 };
 
-export const logout = async (req, res) => {
-  const {} = req.params;
-  res.send("Logout route");
-};
+export const getUser = (req, res) => {};
+
+export const logout = async (req, res) => {};
