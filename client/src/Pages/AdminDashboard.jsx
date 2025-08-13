79ef9f084ced880aacbdd8cae3dea6fb3563ba11
Mark-crypto <mark.onyango@strathmore.edu>
2025-08-13 05:11:47 +0300

    Add admin dashboard

--- client/src/Pages/AdminDashboard.jsx
+++ client/src/Pages/AdminDashboard.jsx
@@ -1,29 +1,43 @@
-import axiosInstance from "../axiosInstance.js"
+import axiosInstance from "../axiosInstance.js";
 import { useQuery } from "@tanstack/react-query";
-import Loading from "../components/Loading.jsx"
+import Loading from "../components/Loading.jsx";
+import UserCard from "@/components/UserCard.jsx";
+import UserTable from "@/components/UserTable.jsx";
 
+const userInfo = [
+  { groupName: "Total Users", total: 50 },
+  { groupName: "Active Users", total: 40 },
+  { groupName: "Inactive Users", total: 10 },
+];
+const AdminDashboard = () => {
+  // const { data, error, isLoading } = useQuery({
+  //   queryKey: ["admin-dashboard"],
+  //   queryFn: async () => {
+  //     return await axiosInstance.get("/admin-dashboard");
+  //   },
+  // });
 
-const AdminDashboard = ()=>{
-  const { data, error, isLoading } = useQuery({
-    queryKey: ["admin-dashboard"],
-    queryFn: async () => {
-      return await axiosInstance.get("/admin-dashboard");
-    },
-  });
-
-  if (isLoading) {
-    return <Loading />;
-  }
-  if (error) {
-    toast.error("Something went wrong");
-  }
-return(
+  // if (isLoading) {
+  //   return <Loading />;
+  // }
+  // if (error) {
+  //   toast.error("Something went wrong");
+  // }
+  return (
     <>
-    <h1>Hello, Admin</h1>
-    <p> I will display: users, reports, and possibly logs</p>
+      <h1>Hello, Admin</h1>
+      <div>
+        <UserCard />
+      </div>
+      <div>
+        <UserTable />
+      </div>
+      <div>
+        <p>Graph: line graph of users joining over the year</p>
+      </div>
+      <p> I will display: users, reports, and possibly logs</p>
     </>
-)
-}
-
+  );
+};
 
 export default AdminDashboard;
---
+++ client/src/components/UserTable.jsx
@@ -0,0 +1,9 @@
+const UserTable = () => {
+  return (
+    <>
+      <div>UserTable</div>
+      <p>rows: no, name, email, status, action</p>
+    </>
+  );
+};
+export default UserTable;
