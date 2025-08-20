9d4ab5b94970ab72f6c453b5f065f7deeb8a17aa
Mark-crypto <mark.onyango@strathmore.edu>
2025-08-20 06:32:03 +0300

    build admin dashboard

--- client/src/Pages/AdminDashboard.jsx
+++ client/src/Pages/AdminDashboard.jsx
@@ -6,12 +6,6 @@ import UserTable from "@/components/UserTable.jsx";
 
 //npm install chart.js react-chartjs-2
 
-
-// const userInfo = [
-//   { groupName: "Total Users", total: 50 },
-//   { groupName: "Active Users", total: 40 },
-//   { groupName: "Inactive Users", total: 10 },
-// ];
 const AdminDashboard = () => {
   const { data, error, isLoading } = useQuery({
     queryKey: ["admin-dashboard"],
@@ -19,7 +13,7 @@ const AdminDashboard = () => {
       return await axiosInstance.get("/admin-dashboard");
     },
   });
-  console.log(data.data);
+
   if (isLoading) {
     return <Loading />;
   }
@@ -27,10 +21,28 @@ const AdminDashboard = () => {
     console.log("Something went wrong");
   }
   return (
-  <div className="admin-dashboard">
-    <h1>Hello, Admin</h1>
-
-    <section>
+    <div className="admin-dashboard">
+      <h1>Hello, Admin</h1>
+      {data?.data?.data.map((user) => {
+        return (
+          <div key={user.user_id}>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </div>
+        );
+      })}
+      <div>
+        <h1>
+          Rows: user_id, name, email,status, role,occupation, age,goal,
+          created_at
+        </h1>
+        <p>Three cards: total users, active, inactive</p>
+        <p>growth rate:user to month</p>
+        <p>Ability to revoke user permissions</p>
+        <p>Name, job, goal</p>
+        <p>User to roles</p>
+      </div>
+      {/* <section>
       <UserCard userInfo={userInfo} />
     </section>
 
@@ -48,10 +60,9 @@ const AdminDashboard = () => {
 
     <section>
       <AuditLogs logs={data?.logs} />
-    </section>
-  </div>
-);
-
+    </section> */}
+    </div>
+  );
 };
 
 export default AdminDashboard;
